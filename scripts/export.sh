f99457965c1da71cc36d5902876b2b1843a6e53e
John Robinson <soco@uw.edu>
2015-10-11 14:18:00 -0700

    add export helper script

---
+++ scripts/export.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+OUTDIR=$1
+
+do_export() {
+    filename="$1$2_$3_$4.csv"
+    echo "writing... $filename"
+  
+    echo "--codescheme $5 $2 $4_$3 > $OUTDIR/$filename" 
+    python manage.py export --codescheme $5 $2 $2_$3 > $OUTDIR/$filename
+}
+
+if [$# -ne 1]:
+    echo "usage: `basename $0` <outdir>"
+    exit 1
+fi
+
+
+
+for dataset in 1 2
+do
+    do_export mt_ andu $dataset and AND
+    do_export mt_ andu $dataset u Uncertainty
+    do_export mt_ and $dataset and AND
+    do_export mt_ u $dataset u Uncertainty 
+done
+
+
